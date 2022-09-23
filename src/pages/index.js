03237dc0fcfb6a1052b64541245fe822017316d1
git-masterdev <panda_baby130@outlook.com>
2022-09-23 02:57:30 -0700

    Profile.jsx added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Home from "./Home";
+import Profile from "./Profile"
 import NoResult from "./NoResult";
 
 
 const routes = [
   { path: '/', component: <Home />},
+  { path: '/profile', component: <Profile />},
   { path: '/no-result', component: <NoResult />},
 ]
 
