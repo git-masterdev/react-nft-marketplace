fceedf977e860be4bbb41d1bc68ad25b0ea780bb
git-masterdev <panda_baby130@outlook.com>
2022-09-29 00:43:48 -0700

    All Images Compress

---
+++ src/assets/fake-data/data-chattrending.js
@@ -0,0 +1,10 @@
+import imgavatar from '../images/avatar/character1.png'
+
+const chatdata = [
+  {
+    username:"fff",
+    useravatar:imgavatar,
+    time:"2 days ago",
+    usermessage:"My ame is Han Ryong Hun."
+  }
+]
