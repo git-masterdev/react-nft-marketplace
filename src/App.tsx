4bf2ddaa3ad57f0154080e42107a68fbe3353144
git-masterdev <panda_baby130@outlook.com>
2022-10-12 11:06:58 -0700

    App.js -> App.tsx typescript version exchange

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {   Routes , Route } from 'react-router-dom';
+import {useState} from 'react';
+import { Routes , Route } from 'react-router-dom';
 import './App.css';
 import routes from './pages/index'
 import Footer from './components/footer/Footer';
@@ -21,9 +21,9 @@ function App() {
           <HeaderStyle2 setAccount={setAccount} setWeb3Api={setWeb3Api} />
           <Routes >
               {
-              routes.map((data,index) => (
-                  <Route onUpdate={() => window.scrollTo(0, 0)} exact={true} path={data.path} element={data.component} key={index} />
-              ))
+                routes.map((data,index) => (
+                    <Route onUpdate={() => window.scrollTo(0, 0)} exact={true} path={data.path} element={data.component} key={index} />
+                ))
               }
           </Routes>
           <Footer/>
--- src/pages/NFTGeneratorUpload.tsx
+++ src/pages/NFTGeneratorUpload.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import '../scss/style.scss';
+
 
 function NFTGeneratorUpload(props){
 
 	return(
-		<div>
-		Hi I am a NFTGeneratorUpload
+		<div className='detail-page pad-t-24'>
+	        <div className="themesflat-container">
+		        <section className="tf-section">
+		            <div className="row mt-21">
+		              	<div className="col-md-12 voomio-title">
+		                  	<div className="">
+			                    <h2 className="tf-title">NFT Generator</h2>
+			                    <div className="heading-line"></div>
+		                  	</div>
+		              	</div>
+		            </div>
+		            <div className="row">
+		            	<div className="col-xs-12 col-sm-3">
+		            		<div className="col-12 mb-5">
+		            			<h4 className="mb-4 spec-color-7">Current NFT</h4>
+		            			<h5 className="spec-color-2">Single-item</h5>
+		            		</div>
+		            		<div className="col-12 nft-setting">
+		            			<h4 className="mb-4 spec-color-7">Settings</h4>
+		            			<h5 className="spec-color-2 mb-3">General</h5>
+		            			<h5 className="spec-color-2 mb-3">Layers</h5>
+		            			<h5 className="spec-color-2 mb-3">Rules</h5>
+		            		</div>
+		            	</div>
+		            	<div className="col-xs-12 col-sm-9">
+		            	
+		            	</div>
+		            </div>
+		        </section>
+			</div>
 		</div>
-		)
+	)
 }
 
 export default NFTGeneratorUpload;
