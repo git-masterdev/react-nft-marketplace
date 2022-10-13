50349e978729c21d47bacbec68da04c5275bd4bd
git-masterdev <panda_baby130@outlook.com>
2022-10-12 22:38:18 -0700

    basic upload

--- src/components/layouts/nftgen/CollectionUpload.tsx
+++ src/components/layouts/nftgen/CollectionUpload.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Button from '@mui/material/Button';
+import Uploady from "@rpldy/uploady";
+import UploadDropZone from "@rpldy/upload-drop-zone";
+import UploadPreview from "@rpldy/upload-preview";
 
 function CollectionUpload(props){
+
+	const indicatorRef = useRef(null);
+
 	return(
 		<div className="row">
 			<div className="row">
 				<div className="card collection-upload-field">
 					<div className="upload-info">
-						<h5>Drag and drop or browse to choose your collection folder</h5>
-						<Button className="voomio-btn btn-filled" variant="contained" size="large">Choose files...</Button>
+					    <Uploady destination={{url: "http://localhost:3000/upload"}}>
+					        <UploadDropZone 
+					          onDragOverClassName="drag-over"
+					          htmlDirContentParams={{ recursive: true }}
+					        >
+					            <span>Drag&Drop File(s) or Folder(s) Here</span>            
+					        </UploadDropZone>
+					        <UploadPreview/>
+					    </Uploady>
 					</div>
 				</div>
 			</div>
