98ba6fbc8f2794b225edd5a6dbd59b625b78c0e2
git-masterdev <panda_baby130@outlook.com>
2022-10-14 03:34:49 -0700

    Manage One-of-Ones Page Complete!

--- src/components/layouts/nftgen/CollectionManage.tsx
+++ src/components/layouts/nftgen/CollectionManage.tsx
@@ -4,13 +4,15 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import {ManageTokenFilter} from '../Filter';
+import {ManageTokenFilter, TokenFilter} from '../Filter';
 
 import ManageSilde from './ManageSlide';
 
 import liveAuctionData from '../../../assets/fake-data/data-live-auction';
 import {token_manage_filter} from '../../../assets/fake-data/data-token-gallery';
 import {texturedata} from '../../../assets/fake-data/data-collection';
+import {filter_tokens} from '../../../assets/fake-data/data-token-gallery';
+
 
 
 
@@ -37,10 +39,22 @@ export default function CollectionManage(){
 	        <div className="w-100 d-flex justify-content-end">
 	        	<ManageTokenFilter defaultvalue={data.default} filterdata={data.type} />
 	        </div>
-		    <ManageSilde title="Clothes" data={liveAuctionData} />
-		    <ManageSilde title="Texture" data={texturedata} />
+			    <ManageSilde title="Clothes" data={liveAuctionData} />
+			    <ManageSilde title="Texture" data={texturedata} />
 	        </TabPanel>
 	        <TabPanel value="2">
+	        	<div className='row'>
+	        		<h4 className="spec-color-1">Manage</h4>
+	        	</div>
+	        	<div className="themesflat-container d-flex justify-content-center">
+		        	<div className="manage-filter-group">
+			        	{filter_tokens.map((filter,index) => (
+							<div className="flex voomio-just-center mb-2 manage-filter" key={filter.name}>
+								<TokenFilter defaultvalue={filter.default} name={filter.name} filterdata={filter.type}/>
+							</div>
+						))}
+	        		</div>
+				</div>
 	        </TabPanel>
 		</TabContext>
 		</div>
