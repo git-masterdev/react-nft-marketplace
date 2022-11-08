ae68aeb008a5dfec607886a5f28999f4478c65a3
git-masterdev <panda_baby130@outlook.com>
2022-11-08 05:36:35 -0800

    react-animated-router added!

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 import routes from './pages/index'
 import Footer from './components/footer/Footer';
 import HeaderStyle2 from './components/header/HeaderStyle2';
+import { AnimatedRoutes } from 'react-animated-router';
+import 'react-animated-router/animate.css'; 
 
 
 
@@ -11,13 +13,13 @@ export default function App() {
       return(
         <>
           <HeaderStyle2 />
-            <Routes>
+            <AnimatedRoutes>
                 {
                   routes.map((data,index) => (
                       <Route onUpdate={() => window.scrollTo(0, 0)} exact={true} path={data.path} element={data.component} key={index} />
                   ))
                 }
-            </Routes>
+            </AnimatedRoutes>
           <Footer/>
       </>
       )
